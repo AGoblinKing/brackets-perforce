11a474b1f52169e4b384bf1426a357691d74279c
unknown <joshg@joshg-corp.arena.ncwest.ncsoft.corp>
2013-08-23 11:48:06 -0700

    Use getActiveEditor instead of getCurrentDocument to enable inline editing

--- main.js
+++ main.js
@@ -1,18 +1,20 @@
 define(function (require, exports, module) {
     "use strict";
     
-    var DocumentManager = brackets.getModule("document/DocumentManager");
+    var EditorManager   = brackets.getModule("editor/EditorManager"),
+        DocumentManager = brackets.getModule("document/DocumentManager");
 
     brackets.getModule("utils/AppInit").appReady(function () {
         var p4 = require("p4");
 
         $(DocumentManager)
             .on("dirtyFlagChange", function (e, doc) {
-                var filePath   = doc.file.fullPath;
+                var filePath     = doc.file.fullPath,
+                    activeEditor = EditorManager.getActiveEditor();
 
-				if(doc === DocumentManager.getCurrentDocument()) {
-					p4.checkout(filePath).fail(p4.add(filePath));
-				}
+                if(activeEditor && doc === activeEditor.document) {
+                    p4.checkout(filePath).fail(p4.add(filePath));
+                }
             })
             .on("pathDeleted", function (e, path) {
                 p4.delete(path).fail(p4.revert(path));
